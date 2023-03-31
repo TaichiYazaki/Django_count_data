becc2b8f1c20e78bae571d4067905dcc322ba250
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-03-31 15:13:55 +0900

    countの追加

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('count_data/', include('count_data.urls'))
+    path('', include('count_data.urls'))
 ]
--- count_data/views.py
+++ count_data/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from count_data.models import Book
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'main.html')
+
+
+def count(request):
+    count = Book.objects.count()
+    context = {"count": count}
+    return render(request, "main.html", context)
