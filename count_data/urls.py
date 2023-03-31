3b8bc9d589c57acc264dc48fea531f57ecd0e8fe
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-03-31 15:44:26 +0900

    avgの実装

--- count_data/urls.py
+++ count_data/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
-    path('count', views.count)
+    path('count', views.count),
+    path('avg', views.avg)
 ]
--- count_data/views.py
+++ count_data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import Avg
 from count_data.models import Book, Review
 
 # Create your views here.
@@ -16,4 +16,14 @@ def count(request):
         "book_count": book_count,
         "review_count": review_count
     }
+    return render(request, "main.html", context)
+
+
+def avg(request):
+    books_avg_price = Book.objects.aggregate(Avg('price'))
+    # ↓辞書型の値(value)を取り出す方法
+    for book_avg_price in books_avg_price.values():
+        context = {
+            "book_avg_price": book_avg_price
+        }
     return render(request, "main.html", context)
